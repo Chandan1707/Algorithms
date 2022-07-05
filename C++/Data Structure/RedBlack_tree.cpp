bf99aca27e28847dbc2721aebd46be7632b5e93b
chandan <chandan743247@gmail.com>
2022-07-05 13:21:56 +0530

    Red black tree insertion code added

--- C++/Data Structure/RedBlack_tree.cpp
+++ C++/Data Structure/RedBlack_tree.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-#define RED false;
-#define BLACK true;
+#define RED false
+#define BLACK true
 
 class Node {
     public:
@@ -28,13 +28,30 @@ class RedBlackTree {
         void InorderHelper (Node *head) {
             if (head) {
                 InorderHelper (head->left);
-                cout << head->info << " ";
+                cout << "( " << head->info << " , ";
+                cout << (head->color == RED ? "RED" : "BLACK" ) << " ), ";
                 InorderHelper (head->right);
+            }
+        }
+        void PreordetHelper(Node *head) {
+            if (head) {
+                cout << "( " << head->info << " , ";
+                cout << (head->color == RED ? "RED" : "BLACK" ) << " ), ";
+                PreordetHelper (head->left);
+                PreordetHelper (head->right);
             }
         }
     public:
         void inorder () {
+            cout << "Inorder :";
             InorderHelper(root);
+            cout << endl;
+        }
+        void preorder () {
+
+            cout << "Preorder :";
+            PreordetHelper(this->root);
+            cout <<endl;
         }
         void insertNode (int val) {
             Node *newNode = new Node(val);
@@ -111,19 +128,80 @@ class RedBlackTree {
         }
 
         void insertFix (Node *node) {
+            // pointer to right child of grand parent
+            Node *rs; 
+            while (node->parent->color != BLACK){
+                if (node->parent && node->parent->parent) {
+                    if (node->parent->parent->left == node->parent) {
+                        rs = node->parent->parent->right;
 
+                        if (rs && rs->color == RED) {
+                            node->parent->color = rs->color = BLACK;
+                            if (node->parent->parent == this->root){
+                                break;
+                            }
+                            node->parent->parent->color = RED;
+                            node = node->parent->parent;
+                        }
+                        else {
+                            if (node->parent->right == node) {
+                                left_rotate(node->parent);
+                                node = node->left;
+                            }
+                            else {
+                                node->parent->color = BLACK;
+                                node->parent->parent->color = RED;
+                                right_rotate(node->parent->parent);
+                            }
+                        }
+                    }
+                    else {
+                        rs = node->parent->parent->left;
+
+                        if (rs && rs->color == RED) {
+                            node->parent->color = rs->color = BLACK;
+                            if (node->parent->parent == this->root){
+                                break;
+                            }
+                            node->parent->parent->color = RED;
+                            node = node->parent->parent;
+                        }
+                        else {
+                            if (node->parent->left == node) {
+                                right_rotate(node->parent);
+                                node = node->right;
+                            }
+                            else {
+                                node->parent->color = BLACK;
+                                node->parent->parent->color = RED;
+                                left_rotate(node->parent->parent);
+                            }
+                        }
+                    }
+                }
+            }
         }
         
 };
 
 int main() {
     RedBlackTree t1;
-    t1.insertNode(20);
-    t1.insertNode(10);
-    t1.insertNode(30);
-    t1.insertNode(40);
-    t1.insertNode(50);
-    t1.insertNode(60);
+    // t1.insertNode(41);
+    // t1.insertNode(38);
+    // t1.insertNode(31);
+    // t1.insertNode(12);
+    // t1.insertNode(19);
+    // t1.insertNode(8);
 
+    t1.insertNode(2);
+    t1.insertNode(1);
+    t1.insertNode(4);
+    t1.insertNode(5);
+    t1.insertNode(9);
+    t1.insertNode(3);
+    t1.insertNode(6);
+    t1.insertNode(7);
+
+    t1.preorder();
     t1.inorder();
 }
